0a9184c472eb02047a6769b1cc1d08b3982272fe
wemos <support@wemos.cc>
2022-09-11 21:46:31 +0800

    test

--- tools/lolin_tool.py
+++ tools/lolin_tool.py
@@ -8,17 +8,31 @@ import os
 import re
 import subprocess
 
+def get_version_from_git():
+    git_tag = subprocess.check_output(
+            ["git", "describe", "--tags", "--abbrev=0"],
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+        ).strip()
+    git_hash = subprocess.check_output(
+            ["git", "rev-parse", "--short", "HEAD"],
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+        ).strip()
 
+    return git_tag+"-"+git_hash
 
 
 def main():
     dirs=glob.glob("./ports/esp32/build-*")    
     print(dirs)
+    git_ver=get_version_from_git()
+    print(git_ver)
     for d in dirs:
         name=d[d.find("build-")+6:]
         # print(name)
         try:
-            os.rename(d+"/firmware.bin",d+"/firmware-"+name+".bin")
+            os.rename(d+"/firmware.bin",d+"/firmware-"+name+"-"+git_ver+".bin")
         except:
             pass
 
