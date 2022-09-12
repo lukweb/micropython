e64208a10a27f58cdbcfc175f2c17b186135ec03
wemos <support@wemos.cc>
2022-09-12 15:00:02 +0800

    update

--- tools/lolin_tool.py
+++ tools/lolin_tool.py
@@ -8,29 +8,30 @@ import os
 import re
 import subprocess
 
+
 def get_version_from_git():
     git_tag = subprocess.check_output(
-            ["git", "describe"],
-            stderr=subprocess.STDOUT,
-            universal_newlines=True,
-        ).strip()
-
+        ["git", "describe"],
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+    ).strip()
 
     return git_tag
 
 
 def main():
-    dirs=glob.glob("./ports/esp32/build-*")    
+    dirs = glob.glob("./ports/esp32/build-*")
     print(dirs)
-    git_ver=get_version_from_git()
+    git_ver = get_version_from_git()
     print(git_ver)
     for d in dirs:
-        name=d[d.find("build-")+6:]
+        name = d[d.find("build-") + 6 :]
         # print(name)
         try:
-            os.rename(d+"/firmware.bin",d+"/firmware-"+name+"-"+git_ver+".bin")
+            os.rename(d + "/firmware.bin", d + "/firmware-" + name + "-" + git_ver + ".bin")
         except:
             pass
+
 
 if __name__ == "__main__":
     main()
