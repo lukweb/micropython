ce5917d0d10f1191408efbfbdd9d48574d8c7e7d
wemos <support@wemos.cc>
2023-03-17 15:38:30 +0800

    del yml

--- ports/esp32/boards/GENERIC_S3_SPIRAM/mpconfigboard.cmake
+++ ports/esp32/boards/GENERIC_S3_SPIRAM/mpconfigboard.cmake
@@ -4,10 +4,6 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.usb
     boards/sdkconfig.ble
-<<<<<<< HEAD
-    boards/sdkconfig.240mhz
-=======
->>>>>>> upsteam/master
     boards/sdkconfig.spiram_sx
     boards/GENERIC_S3_SPIRAM/sdkconfig.board
 )
