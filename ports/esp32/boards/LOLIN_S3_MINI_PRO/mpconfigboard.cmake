cede34ec6605c8ec3f85aa0491bff86b185f261d
wemos <support@wemos.cc>
2024-06-12 13:26:37 +0800

    test

--- ports/esp32/boards/LOLIN_S3_MINI_PRO/mpconfigboard.cmake
+++ ports/esp32/boards/LOLIN_S3_MINI_PRO/mpconfigboard.cmake
@@ -13,5 +13,11 @@ set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
 
 # set(USER_C_MODULES ${GC9A01_MPY_DIR}/gc9a01_mpy/src/micropython.cmake)
 
-set(USER_C_MODULES ${MICROPY_DIR}/gc9a01_mpy/src/micropython.cmake)
+if (GC9A01_MPY_DIR)
+    set(USER_C_MODULES ${GC9A01_MPY_DIR}/gc9a01_mpy/src/micropython.cmake)
+else ()
+    set(USER_C_MODULES ${MICROPY_DIR}/../gc9a01_mpy/src/micropython.cmake)
+endif()
+
+
 
