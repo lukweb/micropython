d88de12fff06bafe09b485889eacd405c44659c8
wemos <support@wemos.cc>
2024-06-12 13:12:14 +0800

    test

--- tools/ci_lolin.sh
+++ tools/ci_lolin.sh
@@ -194,8 +194,9 @@ function ci_esp8266_build {
 function ci_gc9a01_mpy_setup {
     git clone https://github.com/wemos/gc9a01_mpy.git
 
-    export GC9A01_MPY_DIR=$(pwd)
-    echo GC9A01_MPY_DIR
+    # export GC9A01_MPY_DIR=$(pwd)
+    echo "GC9A01_MPY_DIR=$(pwd)" >> $GITHUB_ENV
+    echo $GC9A01_MPY_DIR
     # find ~/ -name "gc9a01*"
     # find ~/ -name "micropython"
 }
